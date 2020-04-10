9f28feb26d48573fa3262e93a43c7aa3f4d72fca
Sagor Sarker <sagorhem3532@gmail.com>
2020-04-10 19:43:06 +0600

    Update bnbert.py

--- bntransformer/bnbert.py
+++ bntransformer/bnbert.py
@@ -1,1 +1,20 @@
 
+from transformers import BertModel, BertTokenizer
+#import torch
+
+class Tokenizer(object):
+    def __init__(self):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+
+    def tokenize(self, text):
+        tokens = self.tokenizer.tokenize(text)
+
+        return tokens
+
+    def encode(self, text):
+        encode_ids = self.tokenizer.encode(text, add_special_tokens=True)
+        return encode_ids
+
+    def decode(self, encode_id_list):
+        decode_text = self.tokenizer.decode(encode_id_list)
+        return decode_text
