ad449581864ac3eeffc0a8b63e7ead149a822bb6
sagorbrur <sagorhem3532@gmail.com>
2021-06-03 15:12:15 +0600

    updated tests.py

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ class TestTokenizer(unittest.TestCase):
     
     def test_tokenizer(self):
         tokenizer = BanglaTokenizer()
-        tokens = tokenizer.tokenize('আমি ভাত খাই।')
+        tokens = tokenizer.tokenize("আমি বাংলায় গান গাই ।")
         self.assertEqual(tokens, ['আমি', 'বাংলা', '##য', 'গান', 'গাই', '।']) 
 
 if __name__ == '__main__':
